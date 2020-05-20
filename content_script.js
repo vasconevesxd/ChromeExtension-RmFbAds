fd8256e4472ba0087723fcc76089626d01777ba3
vasconevexd <vasco.neves98@gmail.com>
2020-05-20 11:51:22 +0100

    First Commit

---
+++ content_script.js
@@ -0,0 +1,5 @@
+setTimeout(() => {  
+    let block = document.getElementsByClassName("ahb00how o9ndlxrr fer614ym dp1hu0rb pzfvarvs buofh1pr ftbm7790")[0];
+    let element =  block.getElementsByClassName("cxgpxx05")[0];
+    element.firstElementChild.style.display = "none";
+}, 2000);
